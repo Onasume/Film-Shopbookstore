2692c1b1c1b76f758dccde23148832725969c7ed
onasume <filmzaza23@gmail.com>
2016-06-26 13:20:53 +0700

    All tent finish user and password

--- app/src/main/java/com/example/onasu/filmshopbookstore/MainActivity.java
+++ app/src/main/java/com/example/onasu/filmshopbookstore/MainActivity.java
@@ -1,17 +1,123 @@
 package com.example.onasu.filmshopbookstore;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    //ประกาศตัวแปร
+
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+    private static final String urlJson = "http://swiftcodingthai.com/25JUN/get_User_Film.php";
+    private boolean statusABoolean = true;
+    private String truepasswordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
     } // Main Method
+
+        //Inner Class การสร้าง class ซ้อน class
+
+    private class userTable extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJson).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.e("25JUN", "e ==>" + e.toString());
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+                super.onPostExecute(s); //ON post
+
+            Log.d("26JUN", "JSON ==>" +s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i=0; i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    if (userString.equals(jsonObject.getString("User"))) {
+
+                        statusABoolean = false;
+                        truepasswordString = jsonObject.getString("password");
+                    }
+
+                }  //for
+
+                if (statusABoolean) {
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.mydialog(MainActivity.this, "ไม่มี user", "ไม่มี" + userString + "นี้");
+                } else if (passwordString.equals(truepasswordString)) {
+
+                    Intent intent = new Intent(MainActivity.this, Showbook.class);
+                    startActivity(intent);
+
+                } else {
+
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.mydialog(MainActivity.this, "Password false",
+                            "กรุณากรอกรหัสผ่านใหม่");
+                }
+
+
+            } catch (Exception e) {
+                Log.d("26JUN", "e onPost ==>" + e.toString());
+            }
+
+        }
+    }
+       // class
+
+    public void clicksignin(View view) {
+
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.mydialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องด้วย");
+
+        } else {
+            userTable userTable = new userTable();
+            userTable.execute();
+        }
+    }
 
     public void clickSiubUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignupActivity.class));
--- app/src/main/java/com/example/onasu/filmshopbookstore/SignupActivity.java
+++ app/src/main/java/com/example/onasu/filmshopbookstore/SignupActivity.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignupActivity extends AppCompatActivity {
 
     //ประกาศตัวแปร
     private EditText NameEditText, UserEditText,
-            PasswordEditText, AddressEditText;
+            PasswordEditText, AdressEditText;
     private String UserString, NameString,
-            PasswordString, AddressString;
+            PasswordString, AdressString;
+    private static final String urlPHP = "http://swiftcodingthai.com/25JUN/Add_user_Film.php";
+    private static final String money = "500";
 
 
     @Override
@@ -23,7 +35,7 @@ public class SignupActivity extends AppCompatActivity {
         UserEditText = (EditText) findViewById(R.id.editText);
         NameEditText = (EditText) findViewById(R.id.editText2);
         PasswordEditText = (EditText) findViewById(R.id.editText3);
-        AddressEditText = (EditText) findViewById(R.id.editText4);
+        AdressEditText = (EditText) findViewById(R.id.editText4);
     }   // Main Method
 
     public void clickSignin(View view) {
@@ -32,13 +44,13 @@ public class SignupActivity extends AppCompatActivity {
         UserString = UserEditText.getText().toString().trim();
         NameString = NameEditText.getText().toString().trim();
         PasswordString = PasswordEditText.getText().toString().trim();
-        AddressString = AddressEditText.getText().toString().trim();
+        AdressString = AdressEditText.getText().toString().trim();
 
 
         //CheckSpace
         if (UserString.equals("") || PasswordString.equals("") ||
-                NameString.equals("") || AddressString.equals("")) {
-                //have Space
+                NameString.equals("") || AdressString.equals("")) {
+            //have Space
             MyAlert myAlert = new MyAlert();
             myAlert.mydialog(this, "มีช่องว่าง", "กรุณากรอกข้อความใหม่");
         } else {
@@ -46,11 +58,36 @@ public class SignupActivity extends AppCompatActivity {
 
             uploaduserToserver();
 
+
         }
 
     } //ClickSign
 
     private void uploaduserToserver() {
 
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("User", UserString)
+                .add("Name", NameString)
+                .add("Password", PasswordString)
+                .add("Adress", AdressString)
+                .add("Money", money)
+                .build();
+
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
     }
 }   // Mainclass
